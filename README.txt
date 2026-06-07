5827cd32035b4c5e37bee8044fcf3c3297d9ff59
ojalaeigo-cmyk <ojalaeigo@gmail.com>
2026-06-07 15:54:15 +0300

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,2 +1,2 @@
-STAR KING ARCADE DIFFICULTY + AUTOFIRE FIXED
-Raskusloogika sama. Lisatud bossi ajal automaattuli.
+STAR KING ARCADE BEST FINAL + NAMES
+Level names restored. Star names restored. Level 30 end message: TO BE CONTINUED.
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='star-king-arcade-difficulty-autofire-fixed-v3';
+const CACHE='star-king-arcade-best-final-names-v1';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
