144444441fa150abccdaa7285f7c61a2a1cdca7d
ojalaeigo-cmyk <ojalaeigo@gmail.com>
2026-06-07 16:40:00 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='star-king-boss-balance-v1';
+const CACHE='star-king-boss-balance-shield-text-v1';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
